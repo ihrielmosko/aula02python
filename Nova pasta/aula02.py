337b3ad3dfc989922b5b244e2a1e7319bacf5d25
ihriel.mosko <80472864+ihrielmosko@users.noreply.github.com>
2024-08-06 21:07:40 -0300

    atividade6

--- Nova pasta/aula02.py
+++ Nova pasta/aula02.py
@@ -1,7 +1,10 @@
 Vendas = [['7/2/2024', 'Tênis', 134, 'Vendedor E'], ['1/2/2024', 'Calça', 77, 'Vendedor B'], ['5/1/2024', 'Boné', 195, 'Vendedor E'], ['2/1/2024', 'Camisa', 85, 'Vendedor E'], ['4/1/2024', 'Boné', 100, 'Vendedor E'], ['2/2/2024', 'Boné', 137, 'Vendedor D'], ['10/2/2024', 'Tênis', 92, 'Vendedor C'], ['1/2/2024', 'Calça', 133, 'Vendedor C'], ['10/1/2024', 'Vestido', 167, 'Vendedor A'], ['5/2/2024', 'Boné', 60, 'Vendedor C'], ['9/2/2024', 'Casaco', 130, 'Vendedor C'], ['8/1/2024', 'Camiseta', 76, 'Vendedor E'], ['10/1/2024', 'Tênis', 197, 'Vendedor B'], ['8/1/2024', 'Tênis', 120, 'Vendedor C'], ['10/2/2024', 'Tênis', 195, 'Vendedor E'], ['2/2/2024', 'Vestido', 141, 'Vendedor B'], ['5/2/2024', 'Casaco', 147, 'Vendedor B'], ['9/2/2024', 'Calça', 147, 'Vendedor B'], ['2/1/2024', 'Vestido', 73, 'Vendedor A'], ['6/1/2024', 'Calça', 137, 'Vendedor B'], ['9/1/2024', 'Tênis', 108, 'Vendedor A'], ['3/2/2024', 'Vestido', 187, 'Vendedor D'], ['5/2/2024', 'Camiseta', 64, 'Vendedor A'], ['1/2/2024', 'Casaco', 120, 'Vendedor B'], ['4/1/2024', 'Calça', 80, 'Vendedor C'], ['9/1/2024', 'Tênis', 98, 'Vendedor E'], ['1/1/2024', 'Casaco', 102, 'Vendedor E'], ['8/1/2024', 'Camisa', 162, 'Vendedor D'], ['9/1/2024', 'Camiseta', 163, 'Vendedor D'], ['4/2/2024', 'Vestido', 153, 'Vendedor E'], ['1/1/2024', 'Boné', 168, 'Vendedor E'], ['10/2/2024', 'Calça', 181, 'Vendedor A'], ['2/1/2024', 'Casaco', 174, 'Vendedor C'], ['6/1/2024', 'Camisa', 197, 'Vendedor A'], ['4/2/2024', 'Camisa', 71, 'Vendedor B'], ['1/1/2024', 'Camisa', 56, 'Vendedor E'], ['10/2/2024', 'Boné', 103, 'Vendedor A'], ['4/2/2024', 'Vestido', 148, 'Vendedor B'], ['5/1/2024', 'Boné', 86, 'Vendedor E'], ['2/1/2024', 'Boné', 166, 'Vendedor D'], ['3/1/2024', 'Camisa', 156, 'Vendedor D'], ['7/2/2024', 'Camiseta', 112, 'Vendedor D'], ['1/1/2024', 'Boné', 93, 'Vendedor C'], ['1/2/2024', 'Calça', 174, 'Vendedor A'], ['5/2/2024', 'Boné', 106, 'Vendedor E'], ['8/1/2024', 'Calça', 188, 'Vendedor A'], ['6/2/2024', 'Casaco', 81, 'Vendedor D'], ['4/1/2024', 'Camiseta', 117, 'Vendedor C'], ['8/1/2024', 'Camiseta', 176, 'Vendedor B'], ['6/2/2024', 'Casaco', 147, 'Vendedor D'], ['2/2/2024', 'Tênis', 186, 'Vendedor B'], ['7/2/2024', 'Casaco', 187, 'Vendedor E'], ['4/2/2024', 'Calça', 114, 'Vendedor E'], ['8/2/2024', 'Vestido', 85, 'Vendedor E'], ['7/1/2024', 'Vestido', 171, 'Vendedor C'], ['5/1/2024', 'Camiseta', 198, 'Vendedor A'], ['6/1/2024', 'Tênis', 128, 'Vendedor D'], ['5/2/2024', 'Casaco', 163, 'Vendedor E'], ['9/2/2024', 'Camiseta', 58, 'Vendedor A'], ['2/1/2024', 'Camiseta', 184, 'Vendedor D'], ['6/2/2024', 'Tênis', 73, 'Vendedor C'], ['8/2/2024', 'Camiseta', 135, 'Vendedor E'], ['8/1/2024', 'Tênis', 115, 'Vendedor C'], ['1/2/2024', 'Calça', 180, 'Vendedor C'], ['9/2/2024', 'Tênis', 84, 'Vendedor A'], ['9/1/2024', 'Vestido', 195, 'Vendedor B'], ['4/2/2024', 'Camisa', 143, 'Vendedor B'], ['4/1/2024', 'Camisa', 99, 'Vendedor D'], ['7/1/2024', 'Tênis', 106, 'Vendedor E'], ['5/2/2024', 'Boné', 115, 'Vendedor A'], ['5/2/2024', 'Camisa', 152, 'Vendedor C'], ['2/2/2024', 'Calça', 145, 'Vendedor B'], ['1/1/2024', 'Vestido', 56, 'Vendedor A'], ['10/2/2024', 'Boné', 139, 'Vendedor D'], ['9/1/2024', 'Casaco', 153, 'Vendedor A'], ['9/2/2024', 'Camiseta', 157, 'Vendedor E'], ['8/2/2024', 'Tênis', 194, 'Vendedor C'], ['4/2/2024', 'Tênis', 164, 'Vendedor B'], ['1/1/2024', 'Tênis', 154, 'Vendedor C'], ['4/1/2024', 'Camiseta', 161, 'Vendedor D'], ['2/1/2024', 'Calça', 175, 'Vendedor A'], ['1/1/2024', 'Casaco', 136, 'Vendedor A'], ['9/1/2024', 'Calça', 140, 'Vendedor A'], ['9/1/2024', 'Casaco', 194, 'Vendedor D'], ['7/2/2024', 'Vestido', 113, 'Vendedor B'], ['10/2/2024', 'Boné', 194, 'Vendedor D'], ['7/2/2024', 'Calça', 139, 'Vendedor C'], ['9/2/2024', 'Camisa', 98, 'Vendedor B'], ['5/1/2024', 'Camiseta', 146, 'Vendedor D'], ['1/1/2024', 'Calça', 75, 'Vendedor D'], ['2/2/2024', 'Camisa', 104, 'Vendedor E'], ['2/1/2024', 'Camisa', 112, 'Vendedor D'], ['8/2/2024', 'Boné', 130, 'Vendedor D'], ['9/1/2024', 'Calça', 145, 'Vendedor D'], ['7/2/2024', 'Camisa', 133, 'Vendedor C'], ['4/2/2024', 'Boné', 97, 'Vendedor D'], ['2/1/2024', 'Boné', 123, 'Vendedor E'], ['8/2/2024', 'Boné', 121, 'Vendedor B'], ['5/1/2024', 'Tênis', 65, 'Vendedor B'], ['2/1/2024', 'Vestido', 161, 'Vendedor B']]
 
+# - - - - -
+
 print("total de vendas: ", len(Vendas))
 
+# - - - - -
 
 ValorMaisCaro = 0
 
@@ -10,6 +13,8 @@ for i in Vendas:
         ValorMaisCaro = i[2]
     
 print ("valor mais caro: ", ValorMaisCaro)
+
+# - - - - -
 
 datas = []
 
@@ -34,6 +39,8 @@ for i in dataCount:
     if dataCount[i] == maiorValorDeData:
         print("data com mais vendas: ", i)
 
+# - - - - -
+
 valores = []
 
 for i in Vendas:
@@ -43,11 +50,58 @@ valores.sort()
 
 print("maiores valores de produto: ", valores[-3:])
 
+# - - - - -
+
 vendedores = {"null"}
 vendedores.remove("null")
-
 
 for i in Vendas:
     vendedores.add(i[3])
 
 print("vendedores: ", vendedores)
+
+# - - - - -
+
+vendedor_vendas = {'Vendedor A': 0, 'Vendedor B': 0, 'Vendedor C': 0, 'Vendedor D': 0, 'Vendedor E': 0}
+
+for i in Vendas:
+    if i[3] == 'Vendedor A':
+        vendedor_vendas['Vendedor A'] += 1
+    if i[3] == 'Vendedor B':
+        vendedor_vendas['Vendedor B'] += 1
+    if i[3] == 'Vendedor C':
+        vendedor_vendas['Vendedor C'] += 1
+    if i[3] == 'Vendedor D':
+        vendedor_vendas['Vendedor D'] += 1
+    if i[3] == 'Vendedor E':
+        vendedor_vendas['Vendedor E'] += 1
+
+
+chave = list(vendedor_vendas)
+valor = list(vendedor_vendas.values())
+x = 0
+y = len(chave)
+
+while x < y-1:
+    if x < len(valor):
+        if valor[x] < valor [x+1]:
+            v1 = valor[x]
+            v2 = chave[x]
+            del valor[x]
+            del chave[x]
+            valor.append(v1)
+            chave.append(v2)
+            x = 0
+        else:
+            x += 1
+
+vendedor_vendas_ordenado = dict()
+
+x = 0
+while x < y:
+    vendedor_vendas_ordenado.update({chave[x]: valor[x]})
+    x += 1
+
+print("por ordem decrescente de vendas: ", vendedor_vendas_ordenado)
+
+# - - - - -
